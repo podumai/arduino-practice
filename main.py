61097036b0fca6af4775ce697a69080e0c243fda
podumai <kirillsm05@gmail.com>
2025-06-21 17:00:38 +0700

    Removed random module and fixed root handler.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import serial
 import flask
 import flask_cors
-import random
 
 serialPort: serial.Serial = serial.Serial("/dev/ttyACM0")
 
@@ -10,11 +9,11 @@ flask_cors.CORS(app)
 
 @app.route("/")
 def rootHandler() -> flask.Response:
-    return flask.render_template("index.html", info=serialPort.readline().decode("utf-8")[:-2])
+    return flask.render_template("index.html")
 
 @app.route("/api/v1/getTemperature")
 def temperatureHandler() -> flask.Response:
-    return flask.jsonify({"LM75A": str(random.randint(0, 30))})
+    return flask.jsonify({"LM75A": serialPort.readline().decode("utf-8")[:-2]})
 
 def main() -> None:
     try:
